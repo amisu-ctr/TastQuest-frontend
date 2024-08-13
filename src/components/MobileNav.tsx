02a70c7a448d945705e074767a4f617f4757dabd
Amisu <hamisurahman@gmail.com>
2024-08-13 06:00:04 +0000

    setup auth0

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,25 +1,44 @@
-import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 import { Separator } from "./ui/separator";
-import { Menu } from "lucide-react";
+import { CircleUserRound, Menu } from "lucide-react";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 const MobileNav = () => {
-    return(
-        <Sheet>
-        <SheetTrigger>
-            <Menu className='text-orange-500'   />
-        </SheetTrigger>
-        <SheetContent className="space-y-3">
-            <SheetTitle>
-                <span>Welcome to TastQuest</span>
-            </SheetTitle>
-            <Separator />
-            <SheetDescription className="flex">
-                <Button className="flex-1 font-bold bg-orange-500">Log in</Button>
-            </SheetDescription>
-        </SheetContent>
-      </Sheet>
-    )
-}
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent className="space-y-3">
+        <SheetTitle>
+          {isAuthenticated ? (
+            <span className="flex items-center font-bold gap-2">
+              <CircleUserRound className="text-orange-500" />
+            </span>
+          ) : (
+            <span>Welcome to TasteQuest </span>
+          )}
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button onClick={() => loginWithRedirect()} className="flex-1 font-bold bg-orange-500">Log in</Button>
+          )}
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
 export default MobileNav;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Auth0ProviderWithNavigate from './auth/auth0ProviderWithNavigate.tsx'
+import "./global.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes.tsx";
 
-import './global.css'
-import {BrowserRouter as Router} from 'react-router-dom'
-import AppRoutes from './AppRoutes.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
     </Router>
-  </StrictMode>,
-)
+  </StrictMode>
+);
